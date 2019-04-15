e166ae44df5df73f5ba43e54b3f3adc9d23dcf03
王飞龙 <wangfeilong@lixin360.com>
2019-04-15 18:21:50 +0800

    "提供api"

---
+++ p-conf-api/src/main/java/com/xiaotiangege/service/api/dto/request/BaseRequest.java
@@ -0,0 +1,11 @@
+package com.xiaotiangege.service.api.dto.request;
+
+/**
+ * @author 小天哥哥 
+ * @mailto 361807535@qq.com  
+ * @Description TODO 
+ * @createDate 2019年4月15日 下午6:19:12 
+ */
+public class BaseRequest {
+
+}
---
+++ p-conf-api/src/main/java/com/xiaotiangege/service/api/service/AAAServiceApi.java
@@ -0,0 +1,11 @@
+package com.xiaotiangege.service.api.service;
+
+/**
+ * @author 小天哥哥 
+ * @mailto 361807535@qq.com  
+ * @Description TODO 
+ * @createDate 2019年4月15日 下午6:20:57 
+ */
+public class AAAServiceApi {
+
+}
