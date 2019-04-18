577347315e075175e3c614876fe94682ea063bff
小天哥哥 <361807535@qq.com>
2019-04-18 18:00:10 +0800

    ""

--- p-conf-api/src/main/java/com/xiaotiangege/service/api/dto/base/Result.java
+++ p-conf-api/src/main/java/com/xiaotiangege/service/api/dto/base/Result.java
@@ -6,17 +6,27 @@ import lombok.Getter;
 import lombok.Setter;
 
 /**
- * @author 小天哥哥 
- * @mailto 361807535@qq.com  
- * @Description TODO 
- * @createDate 2019年4月18日 下午5:24:51 
+ * @author 小天哥哥
+ * @mailto 361807535@qq.com
+ * @Description TODO
+ * @createDate 2019年4月18日 下午5:24:51
  */
 @Getter
 @Setter
 @JsonIgnoreProperties
-public class Result extends BaseDomain{
-	
+public class Result<T> extends BaseDomain {
+
 	private static final long serialVersionUID = -2323428265630782361L;
+
+	private static final String SUCCESS_RESULT = "200";
+	
+	private String code;
+	
+	private String msg;
+	
+	private T data;
+	
+	private String remark;
 
 	
 }
