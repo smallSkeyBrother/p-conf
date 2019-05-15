8029d4778bc5cfa17bf8da727e19795a549d3204
小天哥哥 <361807535@qq.com>
2019-05-15 15:03:27 +0800

    ""

---
+++ p-conf-zkutils/src/main/java/com/xiaotiangege/service/zkutils/PConfChangeWatcher.java
@@ -0,0 +1,19 @@
+package com.xiaotiangege.service.zkutils;
+
+import org.apache.zookeeper.WatchedEvent;
+import org.apache.zookeeper.Watcher;
+
+/**
+ * @author 小天哥哥 
+ * @mailto 361807535@qq.com  
+ * @Description TODO 
+ * @createDate 2019年5月14日 下午4:47:51 
+ */
+public class PConfChangeWatcher implements Watcher{
+
+	@Override
+	public void process(WatchedEvent event) {
+		// TODO Auto-generated method stub
+		
+	}
+}
